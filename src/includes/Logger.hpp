7e05e5978afaed61ea1ebfaab679755fc9de94c7
Jelle van Snik <jellevs@gmail.com>
2021-03-01 14:30:35 +0100

    Logger implemented

---
+++ src/includes/Logger.hpp
@@ -0,0 +1,33 @@
+//
+// Created by jelle on 3/1/2021.
+//
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <vector>
+#include <ostream>
+#include "LogItem.hpp"
+
+namespace log {
+
+	class Logger {
+	private:
+		std::vector<std::ostream *> _streams;
+		Flags::flagType 			_loggerFlags;
+
+	public:
+		Logger(std::ostream &stream);
+		Logger(const std::vector<std::ostream *> &streams);
+		Logger(const Logger &logger);
+
+		void 	setFlags(Flags::flagType flags);
+
+		void	log(const LogItem &item);
+
+		Logger &operator=(const Logger &rhs);
+	};
+
+}
+
+#endif
