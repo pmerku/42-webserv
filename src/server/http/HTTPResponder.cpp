0a2b2127e965c167f2b12513f20efdaf4676d880
Maarten <Marty@Marty-3.local>
2021-03-26 11:41:15 +0100

    update

--- src/server/http/HTTPResponder.cpp
+++ src/server/http/HTTPResponder.cpp
@@ -4,6 +4,8 @@
 
 #include "server/http/HTTPResponder.hpp"
 #include "server/http/ResponseBuilder.hpp"
+#include "env/ENVBuilder.hpp"
+#include "env/env.hpp"
 #include "server/global/GlobalLogger.hpp"
 
 using namespace NotApache;
@@ -22,5 +24,33 @@ void HTTPResponder::generateResponse(HTTPClient &client) {
 			 	);
 	} catch (std::exception &e) {
 		globalLogger.logItem(logger::ERROR, std::string("Response could not be built: ") + e.what());
+	}
+
+	try {
+		CGIenv::env envp;
+		envp.setEnv(CGIenv::ENVBuilder()
+			.AUTH_TYPE(request._headers["AUTHORIZATION"]);
+			.CONTENT_LENGTH(utils::itos(request._body.length()));
+			.CONTENT_TYPE(request._headers["CONTENT_TYPE"]);
+			.GATEWAY_INTERFACE("CGI/1.1");
+			//.PATH_INFO();
+			//.PATH_TRANSLATED();
+			//.QUERY_STRING();
+			//.REMOTE_ADDR();
+			//.REMOTE_IDENT();
+			.REMOTE_USER(request._headers["REMOTE-USER"]);
+			.REQUEST_METHOD(HTTPParser::e_methodMap.find(request._method)->second);
+			.REQUEST_URI(request._uri);
+			//.SCRIPT_NAME();
+			//.SERVER_NAME();
+			//.SERVER_PORT();
+			.SERVER_PROTOCOL("HTTP/1.1");
+			.SERVER_SOFTWARE("HTTP 1.1");
+			.build()
+		);
+		for (int i = 0; envp.getEnv()[i]; i++)
+			std::cout << envp.getEnv()[i] << std::endl;
+	} catch (std::exception &e) {
+		globalLogger.logItem(logger::ERROR, std::string("ENV could not be built: ") + e.what());
 	}
 }
