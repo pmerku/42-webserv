42f91a9302def6560eb9acb1d731160f87c87d33
Maarten <Marty@Marty-3.local>
2021-04-16 16:17:57 +0200

    size_tToString fix

--- src/utils/size_tToString.cpp
+++ src/utils/size_tToString.cpp
@@ -5,8 +5,6 @@
 #include "utils/size_tToString.hpp"
 
 std::string	utils::size_tToString(size_t x) {
-	if (x < 0)
-		return std::string("-") + utils::size_tToString(-x);
 	if (x < 10)
 		return std::string(1, x + '0');
 	return utils::size_tToString(x / 10) + utils::size_tToString(x % 10);
